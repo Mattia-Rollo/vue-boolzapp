e3914c66ebd8f97f75054e3c34c9519473516d45
mattia.rollo <ste.tia@hotmail.it>
2022-11-04 17:52:25 +0100

    fix transition again

--- js/script.js
+++ js/script.js
@@ -236,7 +236,7 @@ createApp({
           onEnter(el, done) {
             gsap.to(el, {
               opacity: 1,
-              height: '1.6em',
+              height: 80,
               delay: el.dataset.index * 0.15,
               onComplete: done
             })
